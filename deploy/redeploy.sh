e4a43e98a532b3f07814797af5fcfa36925cca18
Dmitry Shuranov <dvshur@gmail.com>
2020-05-21 16:15:49 +0300

    added logging to redeploy

--- deploy/redeploy.sh
+++ deploy/redeploy.sh
@@ -1,5 +1,7 @@
 git pull origin aggsig-server;
 docker build -t distributed-signature .;
 img_id=$(docker ps --filter="ancestor:distributed-signature");
+echo "Stopping image "$img_id;
 docket stop $img_id;
 docker run -p 8080:8080 --restart=always -d distributed-signature
+echo "Done";
