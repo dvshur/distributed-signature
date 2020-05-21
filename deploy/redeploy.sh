1055c0a24cbbf980c12e7da017258efbd3352af1
Dmitry Shuranov <dvshur@gmail.com>
2020-05-21 16:31:18 +0300

    fix redeploy

--- deploy/redeploy.sh
+++ deploy/redeploy.sh
@@ -1,7 +1,7 @@
 git_branch=$(git symbolic-ref --short -q HEAD);
 git pull origin $git_branch;
 docker build -t distributed-signature .;
-img_id=$(docker ps --filter="ancestor=distributed-signature");
+img_id=$(docker ps -q --filter="ancestor=distributed-signature");
 echo "Stopping image "$img_id;
 docket stop $img_id;
 docker run -p 8080:8080 --restart=always -d distributed-signature
