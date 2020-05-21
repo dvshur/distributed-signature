ec3265d9c22866d8ca036ea6967a1b10c772d872
Dmitry Shuranov <dvshur@gmail.com>
2020-05-21 11:47:02 +0300

    fixed package name

--- pkg/peer/coordinator_test.go
+++ pkg/peer/coordinator_test.go
@@ -3,49 +3,51 @@ package peer
 import (
 	"crypto/rand"
 	"testing"
+
+	"github.com/dvshur/distributed-signature/pkg/cryptobase"
 )
 
-func randomGE() cryptobaseExtendedGroupElement {
+func randomGE() cryptobase.ExtendedGroupElement {
 	r := make([]byte, 32)
 	rand.Read(r)
 	var r2 [32]byte
 	copy(r2[:], r[:32])
-	var R cryptobaseExtendedGroupElement
+	var R cryptobase.ExtendedGroupElement
 	R.FromBytes(&r2)
 	return R
 }
 
 // Coordinator ..
 func TestSumGeSlice(t *testing.T) {
-	var actualR cryptobaseExtendedGroupElement
+	var actualR cryptobase.ExtendedGroupElement
 
 	// empty slice
-	var empty cryptobaseExtendedGroupElement
-	actualR = sumGeSlice([]cryptobaseExtendedGroupElement{})
+	var empty cryptobase.ExtendedGroupElement
+	actualR = sumGeSlice([]cryptobase.ExtendedGroupElement{})
 	if actualR != empty {
 		t.Errorf("Empty slice does not produce empty result. Got: %d.", actualR)
 	}
 
 	// slice size 1
 	R1 := randomGE()
-	actualR = sumGeSlice([]cryptobaseExtendedGroupElement{R1})
+	actualR = sumGeSlice([]cryptobase.ExtendedGroupElement{R1})
 	if actualR != R1 {
 		t.Errorf("Slice size 1 incorrect result, got: %d, want: %d", actualR, R1)
 	}
 
 	// slice size 2
-	var expectedR, R2 cryptobaseExtendedGroupElement
+	var expectedR, R2 cryptobase.ExtendedGroupElement
 	R2 = randomGE()
-	cryptobaseGeAdd(&expectedR, &R1, &R2)
+	cryptobase.GeAdd(&expectedR, &R1, &R2)
 
-	actualR = sumGeSlice([]cryptobaseExtendedGroupElement{R1, R2})
+	actualR = sumGeSlice([]cryptobase.ExtendedGroupElement{R1, R2})
 
 	if expectedR != actualR {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", expectedR, actualR)
 	}
 
 	// same Ri, but inverted order — test commutativity
-	invertedR := sumGeSlice([]cryptobaseExtendedGroupElement{R2, R1})
+	invertedR := sumGeSlice([]cryptobase.ExtendedGroupElement{R2, R1})
 	if actualR != invertedR {
 		t.Errorf("Sum is not commutative, R12: %d, R21: %d.", actualR, invertedR)
 	}
